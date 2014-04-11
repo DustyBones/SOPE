6d393cd3a7d715f6ff1540526313c55114cfced1
EZSPECIAL <ezspecial@ezspecial-Satellite-L650.(none)>
2014-04-11 18:44:30 +0100

    Works with multiple files but only works one by one

--- monitor.c
+++ monitor.c
@@ -14,34 +14,28 @@ int main (int argc, char *argv[]) {
 		printf("Wrong number of arguments, format is \"monitor time word file1.txt ... fileN.txt\"\n");
 		exit(1);
 	}
-	/*int fd = open(argv[3], O_RDWR | O_APPEND); //hardcoded values for initial testing
-	if (fd < 0) {
-		printf("File not found...\n");
-	}*/
 	int i, pid, fd1[2], fd2[2];
 	pipe(fd1);
 	pipe(fd2);
-	
-	//pid=fork();
-	for(i=3; i<argc; ++i){
+	for(i=2; i<argc; ++i){
 		switch(pid){
-			case -1:
-			
 			case 0:
 				if(fork()>0){
 					//
 				}else{
 					close(fd1[0]);
 					dup2(fd1[1], STDOUT_FILENO);
-					execlp("tail", "tail", "--follow=name", "-n 0", argv[i], NULL); //hardcoded value
+					execlp("tail", "tail", "--follow=name", "-n 0", argv[i], NULL);
 			
 				}
 				if(fork() > 0){
 					//
 				}else{
+					close(fd1[1]);
+					close(fd2[0]);
 					dup2(fd1[0], STDIN_FILENO);
 					dup2(fd2[1], STDOUT_FILENO);
-					execlp("grep", "grep", "--line-buffered", argv[2], NULL); //hardcoded value
+					execlp("grep", "grep", "--line-buffered", argv[2], NULL);
 				}
 				char results[256], customTime[128];
 				time_t currTime;
@@ -49,16 +43,15 @@ int main (int argc, char *argv[]) {
 				struct tm *localTime=localtime (&currTime);
 		
 				strftime(customTime, sizeof(customTime), "%Y-%m-%dT%H:%M:%S", localTime);
-		
+				
+				memset(&results[0], 0, sizeof(results));
 				read(fd2[STDIN_FILENO], &results, sizeof(results));
-		
-				//results[strcspn ( results, "\n" )] = '\0';
-				printf("%s - %s - %c%s", customTime, argv[i], '"', results);//hardcoded value
+
+				printf("%s - %s - %s", customTime, argv[i], results);
 				
 			default:
 				pid=fork();
-				int status;
-				wait(&status);
+				wait(NULL);
 		}
 	}
 	return 0;
